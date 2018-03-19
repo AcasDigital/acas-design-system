61f06f09f8379d35ad30a8efa5ee455014440cb9
Luka Alexander <luka@lukaalexander.com>
2018-03-19 11:21:54 +0000

    Refactor the folder structure into single components and structures. Switch configuration from YAML to JavaScript.

---
+++ src/components/single-components/base/headers/headers.config.js
@@ -0,0 +1,17 @@
+const headerSizes = ['One', 'Two', 'Three', 'Four', 'Five', 'Six'];
+const headerVariants = [];
+
+headerSizes.forEach((headerSize, i) => {
+  headerVariants.push({
+    name: headerSize.toLowerCase(),
+    context: {size: i + 1, text: `Header ${headerSize}`}
+  });
+});
+
+module.exports = {
+  title: 'Headings',
+  collated: true,
+  status: 'wip',
+  variants: headerVariants,
+  default: 'one'
+};
---
+++ src/components/single-components/common/button/button.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: "Button",
+  status: "wip",
+  context: {
+    "button-text": "Print this page"
+  }
+};
