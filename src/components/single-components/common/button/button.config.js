53f257f7780369840c6c269c76af3dedd917d2b2
Luka Alexander <luka@lukaalexander.com>
2018-03-20 11:38:20 +0000

    Add styles and new states to button component

--- src/components/single-components/common/button/button.config.js
+++ src/components/single-components/common/button/button.config.js
@@ -1,7 +1,24 @@
+const states = ['Print', 'Download'];
+const variants = [];
+
+states.forEach(s => {
+  variants.push({
+    name: `${s}`,
+    context: {
+      text: `${s}`,
+      state: s,
+      href: '#'
+    }
+  })
+});
+
 module.exports = {
   title: "Button",
+  collated: true,
   status: "wip",
   context: {
-    "button-text": "Print this page"
-  }
+    "text": "Call to Action",
+    "href": "#"
+  },
+  variants: variants
 };
