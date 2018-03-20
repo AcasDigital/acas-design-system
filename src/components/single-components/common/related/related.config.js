b0f61c050a7d3a0606b642ee4fa443dae2b96cf9
Luka Alexander <luka@lukaalexander.com>
2018-03-20 16:07:22 +0000

    Create related items component

---
+++ src/components/single-components/common/related/related.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: "Related Content",
+  collated: true,
+  status: "wip",
+  context: {
+    items: [
+      {
+        "related-item": {
+          "text": "Related item 1",
+          "href": "#",
+          "target": "_blank"
+        }
+      },
+      {
+        "related-item": {
+          "text": "Related item 2",
+          "href": "#",
+          "target": "_blank"
+        }
+      },
+      {
+        "related-item": {
+          "text": "Related item 3",
+          "href": "#",
+          "target": "_blank"
+        }
+      }
+    ], 
+  }
+};
