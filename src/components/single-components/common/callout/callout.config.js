c84b221e084670f1d02b90cadb12c8de7c3ecf5e
Luka Alexander <luka@lukaalexander.com>
2018-03-20 13:31:59 +0000

    Add the callout box component to the common components library

---
+++ src/components/single-components/common/callout/callout.config.js
@@ -0,0 +1,11 @@
+const variants = [];
+
+module.exports = {
+  title: "Callout Box",
+  collated: true,
+  status: "wip",
+  context: {
+    "text": "For information that needs to stand out or that doesn’t need to follow the flow of content and can appear almost anywhere on the page"
+  },
+  variants: variants
+};
