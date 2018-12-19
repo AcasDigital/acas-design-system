6e5cea0e0a7a6907367bd97112479b0b7b6ee93a
Luka Alexander <luka@lukaalexander.com>
2018-12-19 15:41:23 +0000

    Add navigation component to design system

---
+++ src/components/single-components/navigation/navigation.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  title: "Navigation",
+  collated: true,
+  status: "wip",
+  context: {
+    items: [
+      {
+        "navigation-item": {
+          "title": "Contracts and Pay",
+          "links": ["Pay and Wages", "Hiring Staff"]
+        }
+      },
+      {
+        "navigation-item": {
+          "title": "Leave and Time Off",
+          "links": ["Holiday Entitlement"]
+        }
+      },
+      {
+        "navigation-item": {
+          "title": "Ending Employment",
+          "links": ["Redundancy"]
+        }
+      },
+      {
+        "navigation-item": {
+          "title": "Disciplinary Process",
+          "links": []
+        }
+      }
+    ]
+  }
+};
