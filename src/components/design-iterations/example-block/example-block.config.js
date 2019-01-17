aafd1f012375e98d990915e06ae8f07d38e8463e
Luka Alexander <luka@lukaalexander.com>
2019-01-17 15:38:01 +0000

    Add two variants of the example blocks to the design system

---
+++ src/components/design-iterations/example-block/example-block.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: "Example Block",
+  collated: true,
+  status: "wip",
+  context: {
+    listItems: [
+      {
+        "list-item": {
+          "content": "a care worker agency could ask for female applicants, if the person being cared for is female and she has said she would be uncomfortable in receiving this type of care from a man"
+        }
+      },
+      {
+        "list-item": {
+          "content": "the Catholic church can ask for a priest who is a Catholic"
+        }
+      }
+    ],
+    prose: "You’ve applied for a job. The employer happens to come across your profile on Facebook and discovers a photo that confirms your race, or a post that indicates your political views. It could be argued that even if they didn’t intend to, they’ve formed a perception of you that is not related to your ability to do the job."
+  }
+};
