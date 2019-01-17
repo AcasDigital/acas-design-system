26bbfd497fc604778236f457920399ebda98f9cc
Luka Alexander <luka@lukaalexander.com>
2019-01-17 15:37:33 +0000

    Add new work completed on landing page blocks. Alternative version of the current design and new design altogether.

---
+++ src/components/design-iterations/landing-blocks-alternative/landing-blocks-alternative.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  title: "Landing Page Blocks",
+  collated: true,
+  status: "wip",
+  context: {
+    items: [
+      {
+        "landing-block": {
+          "title": "National Minimum Wage entitlement",
+          "description": "Find out the current minimum wage rates and what to do if you're being underpaid."
+        }
+      },
+      {
+        "landing-block": {
+          "title": "Check if your employer can make deductions from your wages",
+          "description": "What to do if you have not been paid the amount you were expecting."
+        }
+      },
+      {
+        "landing-block": {
+          "title": "Reclaim money owed by an employee",
+          "description": "Check when you can claim money back from an employee, for example overpayments or training costs."
+        }
+      },
+      {
+        "landing-block": {
+          "title": "Final pay when someone leaves a job",
+          "description": "Why someone's pay is often different when they leave a job, including how holiday entitlement affects final pay."
+        }
+      }
+    ]
+  }
+};
