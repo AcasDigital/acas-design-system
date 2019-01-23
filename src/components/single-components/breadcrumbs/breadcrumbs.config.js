20b102c41398185084070ca75bf4cfcc121ec34a
Luka Alexander <luka@lukaalexander.com>
2019-01-23 13:45:43 +0000

    Breadcrumb component work

---
+++ src/components/single-components/breadcrumbs/breadcrumbs.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  title: "Breadcrumbs",
+  collated: true,
+  status: "wip",
+  context: {
+    items: [
+      {
+        "breadcrumb-link": {
+          "title": "Home",
+          "active": false
+        }
+      },
+      {
+        "breadcrumb-link": {
+          "title": "Leave and time off",
+          "active": false
+        }
+      },
+      {
+        "breadcrumb-link": {
+          "title": "Holiday entitlement",
+          "active": false
+        }
+      }
+    ]
+  }
+};
