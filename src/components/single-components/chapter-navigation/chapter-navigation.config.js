ff40349db37c7bdc5454085c9d23291d674ef582
Luka Alexander <luka@lukaalexander.com>
2019-01-23 13:45:09 +0000

    Chapter navigation component work

---
+++ src/components/single-components/chapter-navigation/chapter-navigation.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  title: "Chapter Navigation",
+  collated: true,
+  status: "wip",
+  context: {
+    items: [
+      {
+        "chapter-link": {
+          "title": "Redundancy pay",
+          "active": true
+        }
+      },
+      {
+        "chapter-link": {
+          "title": "Redundancy notice",
+          "active": false
+        }
+      },
+      {
+        "chapter-link": {
+          "title": "How your employer must consult you",
+          "active": false
+        }
+      },
+      {
+        "chapter-link": {
+          "title": "Suitable alternative employment",
+          "active": false
+        }
+      },
+      {
+        "chapter-link": {
+          "title": "Check your redundancy is fair",
+          "active": false
+        }
+      },
+      {
+        "chapter-link": {
+          "title": "Appeal a redundancy decision",
+          "active": false
+        }
+      }
+    ]
+  }
+};
