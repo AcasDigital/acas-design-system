6d485a38bd732fa8f078e7511928af795357780d
Luka Alexander <luka@lukaalexander.com>
2019-01-23 13:44:10 +0000

    Print and download button work

---
+++ src/components/single-components/print-download/print-download.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: "Print and Download",
+  collated: true,
+  status: "wip",
+  context: {
+    items: [
+      {
+        "link": {
+          "title": "Print",
+          "class": "btn-save--print print-opener"
+        }
+      },
+      {
+        "link": {
+          "title": "Download",
+          "class": "btn-save--download download-opener"
+        }
+      }
+    ]
+  }
+};
