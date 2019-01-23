ea0353185adf201c469560915a1bb83a6dd9a2e2
Luka Alexander <luka@lukaalexander.com>
2019-01-23 13:44:32 +0000

    Cookie compliance component work

---
+++ src/components/single-components/cookie-compliance/cookie-compliance.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  title: "Cookie Compliance",
+  collated: true,
+  status: "wip",
+  context: {
+    complianceText: "Acas uses cookies to ensure we give you the best experience and to make the site simpler. By clicking any link on this page you are giving your consent for us to set cookies.",
+    buttons: [
+      {
+        "button": {
+          "title": "Accept cookies",
+          "class": "agree-button eu-cookie-compliance-agree-button"
+        }
+      },
+      {
+        "button": {
+          "title": "Cookie policy",
+          "class": "find-more-button eu-cookie-compliance-more-button find-more-button-processed"
+        }
+      }
+    ]
+  }
+};
