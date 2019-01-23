67fd9262963736cefc61f0bff1eb44455ed79649
Luka Alexander <luka@lukaalexander.com>
2019-01-23 13:43:51 +0000

    Radio buttons - standard and prescribed form design

---
+++ src/components/single-components/radio-buttons/radio-buttons.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  title: "Radio Buttons",
+  collated: true,
+  status: "wip",
+  context: {
+    fieldsetID: "edit-do-you-have-difficulty-speaking-on-the-phone-optional---wrapper",
+    fieldsetClass: "fieldgroup form-composite js-webform-type-radios webform-type-radios js-form-item form-item js-form-wrapper form-wrapper",
+    legendClass: "fieldset-legend",
+    legendTitle: "Do you have difficulty using a phone? (Optional)",
+    description: "We want to make it as easy as possible for you to communicate with us.",
+    optionContainerID: "edit-do-you-have-difficulty-speaking-on-the-phone-optional-",
+    optionContainerClass: "js-webform-radios webform-options-display-one-column",
+    optionName: "do_you_have_difficulty_speaking_on_the_phone_optional_",
+    options: [
+      { 
+        "option":  {
+          "option-container-class": "form-item js-form-item form-type-radio js-form-type-radio",
+          "option-label-class": "control-label option",
+          "option-title": "Yes",
+          "option-id": "edit-do-you-have-difficulty-speaking-on-the-phone-optional-yes",
+          "option-name": "do_you_have_difficulty_speaking_on_the_phone_optional_",
+          "option-class": "form-radio",
+          "option-type": "radio",
+        },
+      },
+      {
+        "option": {
+          "option-container-class": "form-item js-form-item form-type-radio js-form-type-radio",
+          "option-label-class": "control-label option",
+          "option-title": "No",
+          "option-id": "edit-do-you-have-difficulty-speaking-on-the-phone-optional-no",
+          "option-name": "do_you_have_difficulty_speaking_on_the_phone_optional_",
+          "option-class": "form-radio",
+          "option-type": "radio"
+        }
+      }
+    ]
+  }
+};
