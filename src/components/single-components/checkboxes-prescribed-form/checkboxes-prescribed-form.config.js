bcd7d66c4df8ae2b0d4aab111caa3c01b9c42de4
Luka Alexander <luka@lukaalexander.com>
2019-01-23 13:44:51 +0000

    Checkbox component work - regular and prescribed form

---
+++ src/components/single-components/checkboxes-prescribed-form/checkboxes-prescribed-form.config.js
@@ -0,0 +1,117 @@
+module.exports = {
+  title: "Checkboxes",
+  collated: true,
+  status: "wip",
+  context: {
+    fieldsetID: "edit-i-believe-that-my-employer-has-discriminated-against-me-because---gtUtw0Yhp8Y--wrapper",
+    fieldsetClass: "webform-has-field-prefix checkboxes--wrapper fieldgroup form-composite webform-composite-visible-title js-webform-type-checkboxes webform-type-checkboxes js-form-item form-item js-form-wrapper form-wrapper",
+    legendClass: "fieldset-legend",
+    legendTitle: "(Optional) I believe that I've been discriminated against at work because of my:",
+    description: "Please tick all the categories that apply. This will help us give your case to the right person. You can only claim discrimination for these reasons.",
+    optionContainerID: "edit-i-believe-that-my-employer-has-discriminated-against-me-because",
+    optionContainerClass: "js-webform-checkboxes webform-options-display-one-column form-checkboxes",
+    optionName: "do_you_have_difficulty_speaking_on_the_phone_optional_",
+    options: [
+      { 
+        "option":  {
+          "option-container-class": "form-item js-form-item form-type-checkbox js-form-type-checkbox checkbox",
+          "option-label-class": "control-label option",
+          "option-title": "Age",
+          "option-id": "edit-i-believe-that-my-employer-has-discriminated-against-me-because-age",
+          "option-class": "form-checkbox",
+          "option-type": "checkbox"
+        },
+      },
+      {
+        "option": {
+          "option-container-class": "form-item js-form-item form-type-checkbox js-form-type-checkbox checkbox",
+          "option-label-class": "control-label option",
+          "option-title": "Disability",
+          "option-id": "edit-i-believe-that-my-employer-has-discriminated-against-me-because-disability",
+          "option-class": "form-checkbox",
+          "option-type": "checkbox"
+        }
+      },
+      {
+        "option": {
+          "option-container-class": "form-item js-form-item form-type-checkbox js-form-type-checkbox checkbox",
+          "option-label-class": "control-label option",
+          "option-title": "Gender reassignment",
+          "option-id": "edit-i-believe-that-my-employer-has-discriminated-against-me-because-gender-reassignment",
+          "option-class": "form-checkbox",
+          "option-type": "checkbox"
+        }
+      },
+      {
+        "option": {
+          "option-container-class": "form-item js-form-item form-type-checkbox js-form-type-checkbox checkbox",
+          "option-label-class": "control-label option",
+          "option-title": "Marriage or civil partnership",
+          "option-id": "edit-i-believe-that-my-employer-has-discriminated-against-me-because-marriage",
+          "option-class": "form-checkbox",
+          "option-type": "checkbox"
+        }
+      },
+      {
+        "option": {
+          "option-container-class": "form-item js-form-item form-type-checkbox js-form-type-checkbox checkbox",
+          "option-label-class": "control-label option",
+          "option-title": "Pregnancy or maternity",
+          "option-id": "edit-i-believe-that-my-employer-has-discriminated-against-me-because-maternity",
+          "option-class": "form-checkbox",
+          "option-type": "checkbox"
+        }
+      },
+      {
+        "option": {
+          "option-container-class": "form-item js-form-item form-type-checkbox js-form-type-checkbox checkbox",
+          "option-label-class": "control-label option",
+          "option-title": "Race",
+          "option-id": "edit-i-believe-that-my-employer-has-discriminated-against-me-because-race",
+          "option-class": "form-checkbox",
+          "option-type": "checkbox"
+        }
+      },
+      {
+        "option": {
+          "option-container-class": "form-item js-form-item form-type-checkbox js-form-type-checkbox checkbox",
+          "option-label-class": "control-label option",
+          "option-title": "Religion or belief",
+          "option-id": "edit-i-believe-that-my-employer-has-discriminated-against-me-because-religion",
+          "option-class": "form-checkbox",
+          "option-type": "checkbox"
+        }
+      },
+      {
+        "option": {
+          "option-container-class": "form-item js-form-item form-type-checkbox js-form-type-checkbox checkbox",
+          "option-label-class": "control-label option",
+          "option-title": "Sex",
+          "option-id": "edit-i-believe-that-my-employer-has-discriminated-against-me-because-sex",
+          "option-class": "form-checkbox",
+          "option-type": "checkbox"
+        }
+      },
+      {
+        "option": {
+          "option-container-class": "form-item js-form-item form-type-checkbox js-form-type-checkbox checkbox",
+          "option-label-class": "control-label option",
+          "option-title": "Sexual orientation",
+          "option-id": "edit-i-believe-that-my-employer-has-discriminated-against-me-because-sexual-orientation",
+          "option-class": "form-checkbox",
+          "option-type": "checkbox"
+        }
+      },
+      {
+        "option": {
+          "option-container-class": "form-item js-form-item form-type-checkbox js-form-type-checkbox checkbox",
+          "option-label-class": "control-label option",
+          "option-title": "Discrimination claim or support of someone else's discrimination claim",
+          "option-id": "edit-i-believe-that-my-employer-has-discriminated-against-me-because-descrimination",
+          "option-class": "form-checkbox",
+          "option-type": "checkbox"
+        }
+      }
+    ]
+  }
+};
